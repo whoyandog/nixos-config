8c655bbce5e1fd100bd0fb7894dd3d21bc5251cf
Dmitry <dimka360@mail.ru>
2026-04-15 19:43:57 +0000

    added steam base support

---
+++ modules/system/steam/default.nix
@@ -0,0 +1,14 @@
+{ pkgs, ... } : {
+
+    services.xserver.enable = true;
+
+    programs.steam = {
+        enable = true;
+    };
+
+    programs.xwayland.enable = true;
+
+    environment.systemPackages = with pkgs; [
+        xwayland-satellite 
+    ];
+}

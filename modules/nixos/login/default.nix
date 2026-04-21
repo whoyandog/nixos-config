657917a7545a259126b1db9ac3364966acbfd7a7
Dmitry <dimka360@mail.ru>
2026-04-21 13:27:20 +0500

    make keyring work for VS Code

--- modules/nixos/login/default.nix
+++ modules/nixos/login/default.nix
@@ -11,4 +11,7 @@
   services.greetd = {
     enable = true;
   };
+
+  services.gnome.gnome-keyring.enable = true;
+  security.pam.services.greetd.enableGnomeKeyring = true;
 }
--- profiles/home/dev.nix
+++ profiles/home/dev.nix
@@ -1,4 +1,11 @@
-{ ... }:
+{ pkgs, ... }:
+let
+  code = pkgs.writeShellScriptBin "code" ''
+    exec "${pkgs.vscode}/bin/code" --password-store=gnome-libsecret "$@"
+  '';
+in
 {
-  home.packages = [ ];
+  home.packages = [
+    code
+  ];
 }

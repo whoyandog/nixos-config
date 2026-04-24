132b74d5fd4abde4d8d32c3121418e85786acce1
Dmitry <dimka360@mail.ru>
2026-04-24 15:40:48 +0500

    add custom keyboard-only selection in kitty

--- modules/home/kitty/default.nix
+++ modules/home/kitty/default.nix
@@ -8,6 +8,12 @@
 
       "ctrl+shift+v" = "paste_from_clipboard";
       "ctrl+shift+м" = "paste_from_clipboard";
+
+      # Keyboard-only selection from visible terminal output.
+      "ctrl+shift+e" = "kitten hints --type word --program @";
+      "ctrl+shift+у" = "kitten hints --type word --program @";
+      "ctrl+shift+g" = "kitten hints --type line --program @";
+      "ctrl+shift+п" = "kitten hints --type line --program @";
     };
   };
 }

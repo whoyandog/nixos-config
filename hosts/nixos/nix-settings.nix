0743a10a6a80fa42b74806211b9c40943d4eb923
Dmitry <dimka360@mail.ru>
2026-05-01 01:30:45 +0500

    add auto garbage collect weekly for deleting files wich older than 14 days

--- hosts/nixos/nix-settings.nix
+++ hosts/nixos/nix-settings.nix
@@ -2,4 +2,11 @@
 {
   nixpkgs.config.allowUnfree = true;
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.auto-optimise-store = true;
+
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 14d";
+  };
 }

82148a81595051d2b87b5375180f2c51cffa8848
Dmitry <dimka360@mail.ru>
2026-05-01 01:58:05 +0500

    refactoting default folders naming for better expirience

--- home/dmitry.nix
+++ home/dmitry.nix
@@ -4,6 +4,20 @@
   home.homeDirectory = "/home/dmitry";
   home.stateVersion = "25.11";
 
+  xdg.userDirs = {
+    enable = true;
+    createDirectories = true;
+
+    desktop = "$HOME/desktop";
+    documents = "$HOME/documents";
+    download = "$HOME/downloads";
+    music = "$HOME/music";
+    pictures = "$HOME/pictures";
+    publicShare = "$HOME/public";
+    templates = "$HOME/templates";
+    videos = "$HOME/videos";
+  };
+
   stylix.targets.qt.enable = false;
   stylix.targets.kde.enable = false;
 

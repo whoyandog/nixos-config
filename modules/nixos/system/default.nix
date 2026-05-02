bc478b57146aae66a3e0c2627e6e33ef956d09fd
Dmitry <dimka360@mail.ru>
2026-05-02 23:28:10 +0500

    add portals forscreen sharing

--- modules/nixos/system/default.nix
+++ modules/nixos/system/default.nix
@@ -1,3 +1,24 @@
-{ ... } : {
+{ pkgs, ... } : {
     services.upower.enable = true;
+
+    # Required for desktop portal authorization prompts in Wayland sessions.
+    security.polkit.enable = true;
+
+    xdg.portal = {
+        enable = true;
+        xdgOpenUsePortal = true;
+
+        # `wlr` provides screencast on wlroots-like compositors; `gtk` handles file chooser.
+        extraPortals = with pkgs; [
+            xdg-desktop-portal-wlr
+            xdg-desktop-portal-gtk
+        ];
+
+        config = {
+            common.default = [ "wlr" "gtk" ];
+            niri."org.freedesktop.impl.portal.FileChooser" = [ "gtk" ];
+            niri."org.freedesktop.impl.portal.ScreenCast" = [ "wlr" ];
+            niri."org.freedesktop.impl.portal.Screenshot" = [ "wlr" ];
+        };
+    };
 }

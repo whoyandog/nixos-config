2c0aeeb30325051e6de5c2368f4b78799ee11f05
Dmitry <dimka360@mail.ru>
2026-05-03 16:23:32 +0500

    add multimedia support

--- modules/home/niri/default.nix
+++ modules/home/niri/default.nix
@@ -3,7 +3,29 @@
 
 	home.packages = [
 		pkgs.playerctl
+		pkgs.mpv
+		pkgs.loupe
 	];
+
+	xdg.mimeApps = {
+		enable = true;
+		defaultApplications = {
+			"image/jpeg" = [ "org.gnome.Loupe.desktop" ];
+			"image/png" = [ "org.gnome.Loupe.desktop" ];
+			"image/webp" = [ "org.gnome.Loupe.desktop" ];
+			"image/gif" = [ "org.gnome.Loupe.desktop" ];
+			"image/svg+xml" = [ "org.gnome.Loupe.desktop" ];
+			"video/mp4" = [ "mpv.desktop" ];
+			"video/x-matroska" = [ "mpv.desktop" ];
+			"video/webm" = [ "mpv.desktop" ];
+			"video/x-msvideo" = [ "mpv.desktop" ];
+			"audio/mpeg" = [ "mpv.desktop" ];
+			"audio/flac" = [ "mpv.desktop" ];
+			"audio/ogg" = [ "mpv.desktop" ];
+			"audio/mp4" = [ "mpv.desktop" ];
+			"audio/x-wav" = [ "mpv.desktop" ];
+		};
+	};
 
 	gtk = {
 		enable = true;

efdb8a7d53225ced4b46ac82f191749cf764abb9
Dmitry <dimka360@mail.ru>
2026-06-02 22:42:53 +0500

    add cn mirror for nix cache

--- hosts/nixos/nix-settings.nix
+++ hosts/nixos/nix-settings.nix
@@ -1,12 +1,23 @@
 { ... }:
 {
   nixpkgs.config.allowUnfree = true;
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  nix.settings.auto-optimise-store = true;
+  nix.settings = {
+    experimental-features = [ "nix-command" "flakes" ];
+    auto-optimise-store = true;
+    connect-timeout = 5;
+    substituters = [
+      "https://mirrors.ustc.edu.cn/nix-channels/store"
+      "https://cache.nixos.org/"
+    ];
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+    ];
+  };
 
   nix.gc = {
     automatic = true;
     dates = "weekly";
     options = "--delete-older-than 14d";
   };
+  
 }

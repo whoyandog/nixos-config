a9dd836bc1c1ffed0fce0f7fc01dc3d2ac80a2ac
Dmitry <dimka360@mail.ru>
2026-06-03 00:29:34 +0500

    comment default nixos cache site

--- hosts/nixos/nix-settings.nix
+++ hosts/nixos/nix-settings.nix
@@ -7,7 +7,7 @@
     connect-timeout = 5;
     substituters = [
       "https://mirrors.ustc.edu.cn/nix-channels/store"
-      "https://cache.nixos.org/"
+      # "https://cache.nixos.org/"
     ];
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="

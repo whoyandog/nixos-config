30bf1a831d9640d5fa9688f27bb0095f8f52758c
dmitry <dmitry@localhost>
2026-07-18 17:04:02 +0500

    turn on wg-ws-proxy

--- hosts/nixos/networking/default.nix
+++ hosts/nixos/networking/default.nix
@@ -9,7 +9,7 @@
   networking.nftables.enable = true;
   networking.networkmanager.enable = true;
   
-  services.tg-ws-proxy.enable = false;
+  services.tg-ws-proxy.enable = true;
 
   local.networking.n8n.enable = false;
 

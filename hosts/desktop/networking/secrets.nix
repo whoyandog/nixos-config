6bc3b6c8b76a9a342ef9a72a08bdc2bbc5069bb4
dmitry <dmitry@localhost>
2026-08-30 16:45:15 +0500

    remove hardcoded username from other files

--- hosts/desktop/networking/secrets.nix
+++ hosts/desktop/networking/secrets.nix
@@ -1,4 +1,4 @@
-{...}: {
-  local.networking.singBox.configPath = "/home/dmitry/.config/secrets/sing-box.json";
-  local.networking.n8n.envFile = "/home/dmitry/.config/secrets/n8n.env";
+{ userName, ...}: {
+  local.networking.singBox.configPath = "/home/${userName}/.config/secrets/sing-box.json";
+  local.networking.n8n.envFile = "/home/${userName}/.config/secrets/n8n.env";
 }

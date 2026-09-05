a5c1edc57790aec8cd44293e0d0862737ecff7fb
whoyandog <dimka360@mail.ru>
2026-09-05 19:45:32 +0500

    add few apps

--- profiles/system/apps/browsers.nix
+++ profiles/system/apps/browsers.nix
@@ -1,5 +1,6 @@
-{...}: {
+{pkgs, inputs, ...}: {
     environment.systemPackages = with pkgs; [
         qutebrowser
+        inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default
     ];
 }
--- profiles/system/apps/media.nix
+++ profiles/system/apps/media.nix
@@ -1,5 +1,6 @@
-{...}: {
+{inputs, ...}: {
     environment.systemPackages = with pkgs; [
-
+        inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default
+        inputs.yandex-music.packages.${pkgs.stdenv.hostPlatform.system}.default
     ];
 }

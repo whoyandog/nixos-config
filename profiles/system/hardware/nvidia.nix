b579d4d4fa7ac03076a637935e58c43775b86103
whoyandog <dimka360@mail.ru>
2026-09-05 18:57:38 +0500

    add wrapper for hardware services

--- profiles/system/hardware/nvidia.nix
+++ profiles/system/hardware/nvidia.nix
@@ -1,28 +1,5 @@
-{config, lib, ...}: {
-  # NVIDIA settings
-  services.xserver.videoDrivers = ["nvidia"];
-
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
-  };
-
-  hardware.nvidia = {
-    modesetting.enable = true;
-    powerManagement.enable = true;
-    powerManagement.finegrained = false;
-    open = false;
-    nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-  };
-
-  # NVIDIA + Wayland
-  environment.variables = {
-    NIXOS_OZONE_WL = "1";
-    LIBVA_DRIVER_NAME = "nvidia";
-    GBM_BACKEND = "nvidia-drm";
-    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-  };
-
-  xdg.portal.config.niri."org.freedesktop.impl.portal.RemoteDesktop" = lib.mkForce ["gnome"];
+{...}: {
+  imports = [
+    ../../../modules/system/hardware/nvidia
+  ];
 }
--- profiles/system/hardware/printing.nix
+++ profiles/system/hardware/printing.nix
@@ -1,13 +1,5 @@
-{ pkgs, ... }: {
-  services.printing = {
-    enable = true;
-    drivers = [ pkgs.epson-escpr ];
-  };
-  
-  hardware.sane.enable = true;
-  users.users.dmitry.extraGroups = [ "scanner" "lp" ];
-
-  environment.systemPackages = [
-    pkgs.simple-scan
+{...}: {
+  imports = [
+    ../../../modules/system/hardware/printing.nix
   ];
 }

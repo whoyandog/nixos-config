99c27df5d4292d92ad26b9982bf2dd89180760f2
whoyandog <dimka360@mail.ru>
2026-09-14 05:10:42 +0500

    added declarated data for github and added hardware-configuration for tablet

--- modules/user/cli/git/default.nix
+++ modules/user/cli/git/default.nix
@@ -1,12 +1,11 @@
 {...}: {
   programs.git = {
     enable = true;
+    userName = "whoyandog";
+    userEmail = "dimka360@mail.ru";
     settings = {
       user.useConfigOnly = true;
       init.defaultBranch = "main";
     };
-    includes = [
-      {path = "~/.config/git/identity.inc";}
-    ];
   };
 }
